74a0dbee7652ec5c716f5746637c14500b9e0559
AngerOverApathy <violasian@gmail.com>
2023-11-17 18:00:56 -0800

    added styling to hero

--- airbnb-clone/src/components/Hero.jsx
+++ airbnb-clone/src/components/Hero.jsx
@@ -3,8 +3,11 @@ import photoGrid from "../assets/photo-grid.png";
 
 export default function Hero() {
     return(
-        <div className="hero">
+        <section className="hero">
             <img className='photogrid' src={photoGrid} alt="photo grid" />
-        </div>
+            <h1 className="hero-header">Online Experiences</h1>
+            <p className="hero-blurb">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
     )
 }
