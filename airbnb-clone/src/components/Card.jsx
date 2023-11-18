2b58ab160af5dccc1dbdcd9976b2ab00d059f247
AngerOverApathy <violasian@gmail.com>
2023-11-17 19:41:18 -0800

    completed first card

--- airbnb-clone/src/components/Card.jsx
+++ airbnb-clone/src/components/Card.jsx
@@ -6,18 +6,14 @@ export default function Card() {
     return(
         <div className="card">
             <img src={katie} className="katie" alt='katie zaferes'/>
-            
-            <div className="card-info">
-        
                 <div className="card-footer">
                     <img src={star} className="star" />
                     <p className="card-rating">4.98</p>
                     <p className="card-reviews"> (56) · USA</p>
                 </div>
-                
-                <p className="card-blurb">Life Lessons With Katie Zaferes</p>
-                <p><b>From $136</b> / person</p>
-            </div>
+            <p className="card-blurb">Life Lessons With Katie Zaferes</p>
+            <p className="price"><b>From $136</b> / person</p>
         </div>
+
     )
 }
