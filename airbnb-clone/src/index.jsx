28ba8bfcf1ea1984072a9f89bea288c25d3fec71
AngerOverApathy <violasian@gmail.com>
2023-11-18 17:41:25 -0800

    fixed app.js

--- airbnb-clone/src/index.jsx
+++ airbnb-clone/src/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
-import Navbar from './components/Navbar.jsx'
-import Hero from './components/Hero.jsx'
-import Card from './components/Card.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='page'>
-    <Navbar />
-    <Hero />
-    <Card />
+    <App />
   </div>,
 )
