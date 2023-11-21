32f663b9d64d936361adbcd26870eb67f4d06f9b
AngerOverApathy <violasian@gmail.com>
2023-11-20 21:12:22 -0800

    created sidescroll

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -2,21 +2,30 @@ import React from "react"
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Card from './components/Card.jsx'
-import katie from "./assets/katie-zaferes.png";
+import data from './data.jsx'
 
 export default function App() {
+
+    const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })         
+
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card 
-                img={katie}
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+            <div className="cards-container"> {/* Added container */}
+                {cards}
+            </div>
         </div>
     )
 }
--- airbnb-clone/src/components/Card.jsx
+++ airbnb-clone/src/components/Card.jsx
@@ -4,14 +4,14 @@ import star from "../assets/star.png";
 export default function Card(props) {
     return(
         <div className="card">
-            <img src={props.img} className="katie" alt='katie zaferes'/>
+            <img src={props.img} className="katie"/>
                 <div className="card-footer">
                     <img src={star} className="star" />
                     <p className="card-rating">{props.rating}</p>
-                    <p className="card-reviews"> ({props.reviews}) · {props.country}</p>
+                    <p className="card-reviews"> {props.reviewCount} · {props.location}</p>
                 </div>
             <p className="card-blurb">{props.title}</p>
-            <p className="price"><b>${props.price}</b> / person</p>
+            <p className="price"><b>{props.price}</b> / person</p>
         </div>
 
     )
