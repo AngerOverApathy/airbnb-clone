937d986adc2cce22bb36806f39bad954919e8b70
AngerOverApathy <violasian@gmail.com>
2023-11-20 21:48:13 -0800

    page complete

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -5,21 +5,14 @@ import Card from './components/Card.jsx'
 import data from './data.jsx'
 
 export default function App() {
-
     const cards = data.map(item => {
         return (
-            <Card 
+            <Card
                 key={item.id}
-                img={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
-                openSpots={item.openSpots}
+                item={item}
             />
         )
-    })         
+    })               
 
     return (
         <div>
--- airbnb-clone/src/components/Card.jsx
+++ airbnb-clone/src/components/Card.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import star from "../assets/star.png";
 
-export default function Card(props) {
 
+export default function Card(props) {
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     }
 
+
     return(
         <div className="card">
-            <div className="badge">{badgeText}</div>
-            <img src={props.img} className="katie"/>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={props.item.coverImg} className="katie"/>
                 <div className="card-footer">
                     <img src={star} className="star" />
-                    <p className="card-rating">{props.rating}</p>
-                    <p className="card-reviews"> ({props.reviewCount}) · {props.location}</p>
+                    <p className="card-rating">{props.item.stats.rating}</p>
+                    <p className="card-reviews"> ({props.item.stats.reviewCount}) · {props.item.location}</p>
                 </div>
-            <p className="card-blurb">{props.title}</p>
-            <p className="price"><b>{props.price}</b> / person</p>
+            <p className="card-blurb">{props.item.title}</p>
+            <p className="price"><b>{props.item.price}</b> / person</p>
         </div>
 
     )
